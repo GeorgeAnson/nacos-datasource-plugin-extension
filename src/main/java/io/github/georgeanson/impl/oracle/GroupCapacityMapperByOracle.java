ce0e0fd7b302c94771a3bda377304607df70161e
anson <georgeanson@foxmail.com>
2024-02-19 09:14:28 +0800

    GROUP_CAPACITY表配置

--- src/main/java/io/github/georgeanson/impl/oracle/GroupCapacityMapperByOracle.java
+++ src/main/java/io/github/georgeanson/impl/oracle/GroupCapacityMapperByOracle.java
@@ -14,50 +14,50 @@ import io.github.georgeanson.constant.DataSourceConstantExtension;
 public class GroupCapacityMapperByOracle extends AbstractMapper implements GroupCapacityMapper {
     @Override
     public String insertIntoSelect() {
-        return "INSERT INTO group_capacity (group_id, quota, `usage`, `max_size`, max_aggr_count, max_aggr_size,gmt_create,"
-                + " gmt_modified) SELECT ?, ?, count(*), ?, ?, ?, ?, ? FROM config_info";
+        return "INSERT INTO GROUP_CAPACITY (GROUP_ID, QUOTA, `USAGE`, `MAX_SIZE`, MAX_AGGR_COUNT, MAX_AGGR_SIZE,GMT_CREATE,"
+                + " GMT_MODIFIED) SELECT ?, ?, COUNT(*), ?, ?, ?, ?, ? FROM CONFIG_INFO";
     }
 
     @Override
     public String insertIntoSelectByWhere() {
-        return "INSERT INTO group_capacity (group_id, quota,`usage`, `max_size`, max_aggr_count, max_aggr_size, gmt_create,"
-                + " gmt_modified) SELECT ?, ?, count(*), ?, ?, ?, ?, ? FROM config_info WHERE group_id=? AND tenant_id = ''";
+        return "INSERT INTO GROUP_CAPACITY (GROUP_ID, QUOTA,`USAGE`, `MAX_SIZE`, MAX_AGGR_COUNT, MAX_AGGR_SIZE, GMT_CREATE,"
+                + " GMT_MODIFIED) SELECT ?, ?, COUNT(*), ?, ?, ?, ?, ? FROM CONFIG_INFO WHERE GROUP_ID=? AND TENANT_ID = ''";
     }
 
     @Override
     public String incrementUsageByWhereQuotaEqualZero() {
-        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ? AND `usage` < ? AND quota = 0";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = `USAGE` + 1, GMT_MODIFIED = ? WHERE GROUP_ID = ? AND `USAGE` < ? AND QUOTA = 0";
     }
 
     @Override
     public String incrementUsageByWhereQuotaNotEqualZero() {
-        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ? AND `usage` < quota AND quota != 0";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = `USAGE` + 1, GMT_MODIFIED = ? WHERE GROUP_ID = ? AND `USAGE` < QUOTA AND QUOTA != 0";
     }
 
     @Override
     public String incrementUsageByWhere() {
-        return "UPDATE group_capacity SET `usage` = `usage` + 1, gmt_modified = ? WHERE group_id = ?";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = `USAGE` + 1, GMT_MODIFIED = ? WHERE GROUP_ID = ?";
     }
 
     @Override
     public String decrementUsageByWhere() {
-        return "UPDATE group_capacity SET `usage` = `usage` - 1, gmt_modified = ? WHERE group_id = ? AND `usage` > 0";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = `USAGE` - 1, GMT_MODIFIED = ? WHERE GROUP_ID = ? AND `USAGE` > 0";
     }
 
     @Override
     public String updateUsage() {
-        return "UPDATE group_capacity SET `usage` = (SELECT count(*) FROM config_info), gmt_modified = ? WHERE group_id = ?";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = (SELECT COUNT(*) FROM CONFIG_INFO), GMT_MODIFIED = ? WHERE GROUP_ID = ?";
     }
 
     @Override
     public String updateUsageByWhere() {
-        return "UPDATE group_capacity SET `usage` = (SELECT count(*) FROM config_info WHERE group_id=? AND tenant_id = ''),"
-                + " gmt_modified = ? WHERE group_id= ?";
+        return "UPDATE GROUP_CAPACITY SET `USAGE` = (SELECT COUNT(*) FROM CONFIG_INFO WHERE GROUP_ID=? AND TENANT_ID = ''),"
+                + " GMT_MODIFIED = ? WHERE GROUP_ID= ?";
     }
 
     @Override
     public String selectGroupInfoBySize() {
-        return "SELECT id, group_id FROM group_capacity WHERE id > ? ROWNUM > ?";
+        return "SELECT ID, GROUP_ID FROM GROUP_CAPACITY WHERE ID > ? ROWNUM > ?";
     }
 
     @Override
